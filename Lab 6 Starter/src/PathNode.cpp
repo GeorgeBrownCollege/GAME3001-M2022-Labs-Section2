4f62f82995c8f95c98a418321586ec4665f0b56f
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2022-07-05 16:03:51 -0400

    modified PathNode Class to draw a small square

--- Lab 6 Starter/src/PathNode.cpp
+++ Lab 6 Starter/src/PathNode.cpp
@@ -1,7 +1,17 @@
 #include "PathNode.h"
+#include "Util.h"
 
 PathNode::PathNode()
 {
+	SetWidth(10);
+	SetHeight(10);
+	GetTransform()->position = glm::vec2(0, 0);
+	GetRigidBody()->isColliding = false;
+	SetType(GameObjectType::PATH_NODE);
+	SetCurrentHeading(0.0f);
+	SetCurrentDirection(glm::vec2(1.0f, 0.0f)); // faces right
+	SetLOSDistance(1000.0f);
+	SetLOSColour(glm::vec4(0, 0.5, 1, 1)); // light blue colour
 }
 
 PathNode::~PathNode()
@@ -9,6 +19,7 @@ PathNode::~PathNode()
 
 void PathNode::Draw()
 {
+	Util::DrawRect(GetTransform()->position - glm::vec2(5, 5), 10, 10, GetLOSColour());
 }
 
 void PathNode::Update()
